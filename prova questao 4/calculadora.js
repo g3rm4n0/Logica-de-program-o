840ed9a0e8d17f2686e3e89af4abeefecad64bde
Germano Ventura <og3rm4n0@gmail.com>
2024-09-26 20:03:35 -0300

    Add files via upload

---
+++ prova questao 4/calculadora.js
@@ -0,0 +1,8 @@
+//let num1 = prompt("Digite um numero")
+//let num2 = prompt("Digete um numero")
+let num1 = 15;
+let num2 = 4;
+let X = 0;
+    
+    x = num1 * num2;
+    console.log("O valor de X = ", x); //Resultado = 60 
