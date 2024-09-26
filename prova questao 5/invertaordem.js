5f57760b643c70f03e986b3cdaa9718ab0ef8814
Germano Ventura <og3rm4n0@gmail.com>
2024-09-26 20:24:12 -0300

    Add files via upload

---
+++ prova questao 5/invertaordem.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Digite um número")
+let num2 = prompt("Digite um número")
+let num3 = prompt("Digite um número")
+
+let array = [num1, num2, num3]
+array.reverse();
+console.log(array)
